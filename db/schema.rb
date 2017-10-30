5876fdf93ff65c31027457407c5d82e4d6fdf9cf
Unai Trecet <ut@12auto.de>
2017-10-30 12:32:00 +0100

    Schema updated.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171028174431) do
+ActiveRecord::Schema.define(version: 20171029130606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,9 +25,12 @@ ActiveRecord::Schema.define(version: 20171028174431) do
   create_table "employees", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "identifier"
+    t.string "identify"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "company_id"
+    t.index ["company_id"], name: "index_employees_on_company_id"
   end
 
+  add_foreign_key "employees", "companies"
 end
