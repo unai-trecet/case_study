f76297c30ad1264c7ec94dd10cbce1c18f0beb5e
Unai Trecet <ut@12auto.de>
2017-10-30 12:32:34 +0100

    Renaming employee field.

--- db/migrate/20171028174431_create_employees.rb
+++ db/migrate/20171028174431_create_employees.rb
@@ -3,7 +3,7 @@ class CreateEmployees < ActiveRecord::Migration[5.1]
     create_table :employees do |t|
       t.string :first_name
       t.string :last_name
-      t.string :identifier
+      t.string :identify
 
       t.timestamps
     end
